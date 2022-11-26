e5357db0c8c30c605917b72ad7caff596c5f75ce
Emimint <emilie.echevin@gmail.com>
2022-11-26 15:55:29 -0500

    implement scroll back up component behavior

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 "use strict";
 
 const navbarElems = document.querySelectorAll(".nav_item");
+const scrollBackButton = document.querySelector(".fixed");
 
+
+/* "NAVBAR" interaction function: */
+// TO BE FIXED!!!
 document.addEventListener("click", function (e) {
   const current = document.querySelector(".selected"); // currently selected navbar link (if defined)
   current?.firstChild && current.classList.remove("selected");
@@ -10,4 +14,12 @@ document.addEventListener("click", function (e) {
       return x.firstChild.classList.add("selected");
     }
   });
+});
+
+
+
+/* "Scrolling back up" function: */
+scrollBackButton.addEventListener("click", function () {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 });
